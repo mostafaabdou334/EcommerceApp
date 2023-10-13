9d7efb0b4c9c1f4ab29f5d7672246c6b015b7796
mostafaabdou334 <mostafaabdou334@gmail.com>
2023-10-13 16:44:37 +0300

    commit2

--- database/database.connection.js
+++ database/database.connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const databaseConnection = async()=>{
 
-    return await mongoose.connect(process.env.DATABASE_url)
+    return await mongoose.connect(process.env.DATABASE_CLoud)
     .then((res) => { console.log("Connection has been established successfully.") })
     .catch((err) => { console.log('Unable to connect to the database:', err) })
 }
