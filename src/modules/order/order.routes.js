d743d7f95169b803e83571781fec99162dfbb00c
mostafaabdou334 <mostafaabdou334@gmail.com>
2023-10-13 18:33:41 +0300

    commit5

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -5,23 +5,24 @@ import { isAuth } from "../../middleware/auth.js";
 import { orderRoles } from "./order.roles.js";
 import { validationCoreFunction } from "../../middleware/validation.js";
 import { cancelOrderSchema, fromCartOrderSchema } from "./order.validationSchema.js";
+import express from 'express'
 
 
 
 
 const router = Router()
 
-router.post('/fromCartOrderCash',isAuth(orderRoles.createOrder),validationCoreFunction(fromCartOrderSchema) ,errorHandler(orderController.fromCartOrderCash))  
-router.post('/fromCartOrderCard',isAuth(orderRoles.createOrder),validationCoreFunction(fromCartOrderSchema) ,errorHandler(orderController.fromCartOrderCard))  
-router.post('/webhook', express.raw({ type: 'application/json' }),errorHandler(orderController.webhook))
+router.post('/fromCartOrderCash', isAuth(orderRoles.createOrder), validationCoreFunction(fromCartOrderSchema), errorHandler(orderController.fromCartOrderCash))
+router.post('/fromCartOrderCard', isAuth(orderRoles.createOrder), validationCoreFunction(fromCartOrderSchema), errorHandler(orderController.fromCartOrderCard))
+router.post('/webhook', express.raw({ type: 'application/json' }), errorHandler(orderController.webhook))
 
-router.patch('/cancelOrder',isAuth(orderRoles.createOrder),validationCoreFunction(cancelOrderSchema) ,errorHandler(orderController.cancelOrder))  
-  
+router.patch('/cancelOrder', isAuth(orderRoles.createOrder), validationCoreFunction(cancelOrderSchema), errorHandler(orderController.cancelOrder))
+
 // router.get('/successOrder' ,errorHandler(orderController.successPayment))  
-router.get('/cancelOrderr' ,errorHandler(orderController.cancelPayment))
- 
+router.get('/cancelOrderr', errorHandler(orderController.cancelPayment))
+
 // router.post('/createOrder',isAuth(orderRoles.createOrder) ,errorHandler(orderController.createOrder)) 
-router.patch('/deliverOrder',isAuth(orderRoles.createOrder) ,errorHandler(orderController.deliverOrder))  
+router.patch('/deliverOrder', isAuth(orderRoles.createOrder), errorHandler(orderController.deliverOrder))
 
 
 
